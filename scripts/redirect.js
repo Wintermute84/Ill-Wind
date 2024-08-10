b1abab8bb77e966ac83022f689943dfd883e352a
Wintermute84 <neuromancerr83@gmail.com>
2024-08-10 23:10:56 +0530

    Accidentally deleted redirect.
    Added redirect file without all the other redirects for buttons and home-links

---
+++ scripts/redirect.js
@@ -0,0 +1,12 @@
+
+function illWind(){
+  count++;
+  if(count===10){
+    window.open("https://open.spotify.com/album/6JE7Xs3YJNAHCggVHZNRjR?si=OVvHqhMNS8CfaD1GxFgtfA");
+    console.log('illwind');
+    count=0;
+  }
+}
+
+
+
