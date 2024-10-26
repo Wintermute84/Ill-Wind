b6f421629c820ee49b17d9128a70476d5739858c
Wintermute84 <neuromancerr83@gmail.com>
2024-10-27 00:00:22 +0530

    ww

--- scripts/popups.js
+++ scripts/popups.js
@@ -2,6 +2,9 @@ export function renderPopups(){
   document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.js-modal-links').forEach((modalLink)=>{
       modalLink.addEventListener('click',()=>{
+        localStorage.setItem(oprio,1);
+        let j = localStorage.getItem('oprio');
+        console.log(j);
         const modalType = modalLink.dataset.modalId;
         let modalHtml = ``;
         if(modalType === '1'){
